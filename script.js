2cdd53a539b96da3ef689210ecbd03f1049b0787
Prince Mayor <princemayor5606@gmail.com>
2024-09-23 01:49:42 +0100

    THECALCULATOR

---
+++ script.js
@@ -0,0 +1,76 @@
+let display = document.getElementById('display')
+
+function one (){
+    display.value += '1'
+}
+
+function two (){
+    display.value += '2'
+}
+
+function three (){
+    display.value += '3'
+}
+
+function four (){
+    display.value += '4'
+}
+
+function five (){
+    display.value += '5'
+}
+
+function six (){
+    display.value += '6'
+}
+
+function seven (){
+    display.value += '7'
+}
+
+function eight (){
+    display.value += '8'
+}
+
+function nine (){
+    display.value += '9'
+}
+
+function zero (){
+    display.value += '0'
+}
+
+function zerozero (){
+    display.value += '00'
+}
+
+function plus (){
+    display.value += '+'
+}
+
+function minus (){
+    display.value += '-'
+}
+
+function division (){
+    display.value += '/'
+}
+
+function multiply (){
+    display.value += '*'
+}
+
+function dot (){
+    display.value += '.'
+}
+
+function ac (){
+    display.value = '' ;
+}
+function del (){
+    displayValue = displayvalue.slice(0, -1);
+}
+
+function equalto (){
+    display.value = eval(display.value);
+}
